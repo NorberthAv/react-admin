cbb3f163c78bba2265a354bcc795e857da6e4d16
navilan <navilan@ks2.tech>
2023-09-06 15:16:30 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { Menu } from './components/Menu';
+import { Content } from './components/Content';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
